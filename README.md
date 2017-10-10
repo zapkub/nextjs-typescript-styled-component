3b89547b5fc6797dd9c494519f6d5d8cbd90977d
Rungsikorn Rungsikavanich <rungsikorn@me.com>
2017-10-10 17:21:14 +0700

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+## Run
+```
+yarn install
+yarn tsc // compile typescript
+yarn next // run nextjs
+```
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import StyledComponents from 'styled-components'
+const styled = StyledComponents
+const Input = styled.input`
+  width: 100px;
+`
+
+export default () => {
+  return (
+    <Input />
+  )
+}
